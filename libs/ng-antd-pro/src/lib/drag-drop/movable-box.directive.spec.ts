ffa7a82fabc75a6ef84023774b7a6fbef869340d
Tony Een <tonyeen@foxmail.com>
2018-10-03 13:47:22 +0800

    删除DragDropModule测试文件

--- libs/ng-antd-pro/src/lib/drag-drop/movable-box.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { MovableBoxDirective } from './movable-box.directive';
-
-// describe('MovableBoxDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new MovableBoxDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
