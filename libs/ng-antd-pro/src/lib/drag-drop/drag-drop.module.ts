83a8eae93694d991447dfd531db837ec21855e87
Tony <tonyeen@foxmail.com>
2018-10-09 22:35:37 +0800

    Clear old drag drop implement

--- libs/ng-antd-pro/src/lib/drag-drop/drag-drop.module.ts
+++ libs/ng-antd-pro/src/lib/drag-drop/drag-drop.module.ts
@@ -1,54 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OverlayModule } from "@angular/cdk/overlay";
-import { ResizableDirective } from './resizable.directive';
-import { DraggableDirective } from './draggable.directive';
-import { ResizableComponent } from './resizable.component';
-import { MovableDirective } from './movable.directive';
-import { MovableBoxDirective } from './movable-box.directive';
-import { MovableAreaDirective } from './movable-area.directive';
-import { DraggableHelperDirective } from './draggable-helper.directive';
-import { SortableListDirective } from './sortable-list.directive';
-import { SortableDirective } from './sortable.directive';
-import { DroppableDirective } from './droppable.directive';
-import { DropzoneDirective } from './dropzone.directive';
-import { DroppableService } from './droppable.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    OverlayModule,
   ],
   declarations: [
-    ResizableDirective,
-    DraggableDirective,
-    ResizableComponent,
-    MovableDirective,
-    MovableBoxDirective,
-    MovableAreaDirective,
-    DraggableHelperDirective,
-    SortableListDirective,
-    SortableDirective,
-    DroppableDirective,
-    DropzoneDirective,
   ],
   exports: [
-    ResizableDirective,
-    DraggableDirective,
-    MovableDirective,
-    MovableBoxDirective,
-    MovableAreaDirective,
-    DraggableHelperDirective,
-    SortableListDirective,
-    SortableDirective,
-    DroppableDirective,
-    DropzoneDirective,
   ],
   entryComponents: [
-    ResizableComponent,
   ],
   providers: [
-    DroppableService,
   ]
 })
 export class DragDropModule { }
